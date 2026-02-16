cb104e681f48b29bc72514c3f28fedafcf259aad
Gabin Kolly <gabin1999@hotmail.com>
2026-02-16 15:04:14 +0100

    use Fisher-Yates to scramble the word

--- src/hooks/useGameState.js
+++ src/hooks/useGameState.js
@@ -32,8 +32,10 @@ const shuffleWord = (word) => {
   let attempts = 0;
 
   do {
-    for (let i = 0; i < 10; i += 1) {
-      shuffled.sort(() => Math.random() - 0.5);
+    shuffled = [...letters];
+    for (let i = shuffled.length - 1; i > 0; i -= 1) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[i]];
     }
 
     attempts += 1;

4481d08cc1c66250bfe9294c14069c3698746960
Gabin <68381468+GabinKolly@users.noreply.github.com>
2026-03-06 15:21:04 +0100

    Adjust main menu order to place Jeu libre second

--- src/components/ModeSelection.tsx
+++ src/components/ModeSelection.tsx
@@ -17,29 +17,29 @@ export default function ModeSelection({
       <div className="mm-menu-selection__buttons">
         <button
           type="button"
-          onClick={onSelectSolo}
+          onClick={onSelectCompetition}
           className="mm-menu-button mm-menu-button--green"
           data-mm-menu-anchor="start"
         >
+          <Trophy className="mm-menu-button__icon" strokeWidth={2.5} />
+          <span>Jeu principal</span>
+        </button>
+        <button
+          type="button"
+          onClick={onSelectSolo}
+          className="mm-menu-button mm-menu-button--yellow"
+        >
           <Play className="mm-menu-button__icon" strokeWidth={2.5} />
-          <span>Solo</span>
+          <span>Jeu libre</span>
         </button>
         <button
           type="button"
           onClick={onSelectMultiplayer}
-          className="mm-menu-button mm-menu-button--yellow"
-        >
-          <Users className="mm-menu-button__icon" strokeWidth={2.5} />
-          <span>Deux joueurs</span>
-        </button>
-        <button
-          type="button"
-          onClick={onSelectCompetition}
           className="mm-menu-button mm-menu-button--red"
           data-mm-menu-anchor="end"
         >
-          <Trophy className="mm-menu-button__icon" strokeWidth={2.5} />
-          <span>Compétition</span>
+          <Users className="mm-menu-button__icon" strokeWidth={2.5} />
+          <span>Deux joueurs</span>
         </button>
       </div>
     </div>
